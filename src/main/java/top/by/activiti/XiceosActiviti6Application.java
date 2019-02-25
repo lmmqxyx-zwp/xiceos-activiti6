494b265b97e6ac170e6083c4527b6ee905ad9f17
zwp <byvaio@outlook.com>
2019-02-25 20:34:21 +0800

    spring boot 2.x集成activiti 6

---
+++ src/main/java/top/by/activiti/XiceosActiviti6Application.java
@@ -0,0 +1,26 @@
+package top.by.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>Title: XiceosActiviti6Application.java</p>
+ * <p>Description: 集成Activiti 6 管理界面</p>
+ * <p>Copyright: Copyright (c) 2018</p>
+ * <p>Company: www.iceos.top</p>
+ *
+ * ----------------------------------------
+ * ----------------------------------------
+ * @author zwp
+ * @date 2019年2月25日
+ *
+ * @version 1.0
+ */
+@SpringBootApplication
+public class XiceosActiviti6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiceosActiviti6Application.class, args);
+	}
+
+}
